837c5df6b3bbae7ed158ba4c128a12a36e678d4d
Josiana <josianasilva655@gmail.com>
2022-12-18 12:07:37 -0300

    Reset e adição de gitignore

--- trabalho.asm
+++ trabalho.asm
@@ -3,7 +3,7 @@ print:
 	LDI ptr           
 	SUB ultimo_caractere
 	JZ  print_linha1
-do:
+
 	LDI ptr           
 	ADD video_end
 	INT output 
@@ -20,15 +20,13 @@ print_linha1:
 	INT output
 
 entrada_dados:
-
 	LDA teclado_end
     INT input
 	
 conversao_entrada:
-	
 	SUB converter       ; AC - 48
-	JN operacao   					; se não houver entrada, pula pra operação	
-	STA n					; n = AC
+	JN operacao   		; se nï¿½o houver entrada, pula pra operaï¿½ï¿½o	
+	STA n				; n = AC
 
 	MOV 0
 	SUB n
@@ -37,14 +35,12 @@ conversao_entrada:
 continue:
 	MOV 1 
 	ADD contador
-	STA contador 					; cont += 1
-	
+	STA contador 		; cont += 1	
 	
 	MOV 2
 	SUB contador
-	JZ mult10 					; se cont = 2 vai pra mult10
+	JZ mult10 			; se cont = 2 vai pra mult10
 	
-
 	LDA n
 	STA acumulador
 	
@@ -56,8 +52,7 @@ condicao:
 	JZ end
 	JMP continue
 	
-mult10:
-	
+mult10:	
 	LDA acumulador
 	SHIFT esquerda
 	SHIFT esquerda
@@ -68,47 +63,43 @@ mult10:
 
 	JMP operacao
 
-operacao:
-		
+operacao:	
 	MOV 1
 	SUB contador
-	JZ dobrar ; if contador = 1 
+	JZ dobrar 			; if contador = 1 
 
 	LDA n
 	ADD acumulador
 	STA acumulador
 
 dobrar:
-
 	LDA acumulador
 	SHIFT esquerda
  	STA dobro
 
-dividindo_1:
-
+operacao2:
 	LDA dobro
-	STA quociente
+	STA parte_inteira
 
-dividindo_2:
-
-	LDA quociente
+op_modulo_10:
+	LDA parte_inteira
 	SUB dez
-	JN print2; quociente < 10
+	JN print2			; parte_inteira < 10
 
-	LDA quociente
+	LDA parte_inteira
 	SUB dez	
-	STA quociente
+	STA parte_inteira
 	
 	MOV 1
-	ADD auxiliar
-	STA auxiliar
-	JMP dividindo_2	
+	ADD modulo
+	STA modulo
+	JMP op_modulo_10	
 
 print2: 
 	LDI ptr2     
 	SUB ultimo_caractere
 	JZ  saida_dados
-do:
+
 	LDI ptr2          
 	ADD video_end
 	INT output 
@@ -120,15 +111,14 @@ do:
 	JMP print2
 
 saida_dados:
-
 	MOV 0
 	LDA video_end
-	ADD auxiliar
+	ADD modulo
 	ADD converter
 	INT output
 
 	LDA video_end
-	ADD quociente
+	ADD parte_inteira
 	ADD converter
 	INT output
 
@@ -136,48 +126,49 @@ print_linha:
 	LDA video_end
 	ADD quebra_linha
 	INT output
-	
+
+reinicializacao_dos_valores:	
 	MOV 0
 	STA n
-	STA auxiliar
+	STA modulo
 	STA acumulador
 	STA contador
 	STA dobro
-	STI ptr2 
 
-	JMP print
+	JMP entrada_dados
+
 end:
 	INT exit
 
 .data
-	;syscall exit
+	
 	exit: DD 25
 
-	dobro: DD 0
-	n: DD 0
-	contador: DD 0
-	acumulador: DD 0
+	dobro: 				DD 0
+	n: 					DD 0
+	contador: 			DD 0
+	acumulador: 		DD 0
 
-	auxiliar: DD 0
-	quociente: DD 0
+	modulo: 			DD 0
+	parte_inteira: 		DD 0
 
-	converter: DD 48
-	quebra_linha: DD 13
-	dez: DD 10
+	converter: 			DD 48
+	quebra_linha: 		DD 13
+	dez: 				DD 10
 
-	mensagem: INITB "Escreva um numero ",0
-	ptr: DD mensagem
-	mensagem2: INITB "O dobro e ",0
-	ptr2: DD mensagem2
-	ultimo_caractere: DD 0
+	mensagem: 			INITB "Escreva um numero ",0
+	ptr: 				DD mensagem
+	mensagem2: 			INITB "O dobro e ",0
+	ptr2: 				DD mensagem2
+	ultimo_caractere: 	DD 0
 
-	video_end: DD 0x000
-	input: DD 20
+	video_end: 			DD 0x000
+	input: 				DD 20
 
-	teclado_end: DD 0x100
-	output: DD 21
+	teclado_end: 		DD 0x100
+	output: 			DD 21
 
-	direita:DD 0
-	esquerda: DD 1
+	direita: 			DD 0
+	esquerda: 			DD 1
 	
 .stack 100
